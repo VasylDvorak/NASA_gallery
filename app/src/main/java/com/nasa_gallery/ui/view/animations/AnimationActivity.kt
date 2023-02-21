5b669f24b3d930f371ea146f08aa538d68fedb79
Vasyl <dvorak1981@yandex.ru>
2023-02-21 21:26:08 +0300

    Движение по траектории

--- app/src/main/java/com/nasa_gallery/ui/view/animations/AnimationActivity.kt
+++ app/src/main/java/com/nasa_gallery/ui/view/animations/AnimationActivity.kt
@@ -1,59 +1,42 @@
 package com.nasa_gallery.ui.view.animations
 
-import android.graphics.Rect
 import android.os.Bundle
-import androidx.transition.ChangeBounds
-import androidx.transition.ChangeImageTransform
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
+
+import android.view.Gravity
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import androidx.transition.Explode
-import androidx.transition.Transition
+import androidx.transition.ArcMotion
+import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
-import androidx.transition.TransitionSet
-import com.nasa_gallery.R
-import com.nasa_gallery.databinding.ActivityAnimationSecondBinding
-import com.nasa_gallery.databinding.ActivityAnimationZoomBinding
+import com.nasa_gallery.databinding.ActivityAnimationTraectoryBinding
 
 class AnimationActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAnimationZoomBinding
-    var isFlag = false
+    private lateinit var binding: ActivityAnimationTraectoryBinding
+    var isFlag = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationZoomBinding.inflate(layoutInflater)
+        binding = ActivityAnimationTraectoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        binding.imageView.setOnClickListener {
+        binding.button.setOnClickListener {
             isFlag = !isFlag
-            val params = it.layoutParams as LinearLayout.LayoutParams
-            val transitionSet = TransitionSet()
-val changeImageTransform = ChangeImageTransform()
-            val changeBounds = ChangeBounds()
-            changeBounds.duration =2000L
-            changeImageTransform.duration =2000L
-            transitionSet.addTransition(changeBounds) // важен порядок
-            transitionSet.addTransition(changeImageTransform)
+            val params = it.layoutParams as FrameLayout.LayoutParams
+            var changeBounds = ChangeBounds()
+            changeBounds.duration = 2000L
+            changeBounds.setPathMotion(ArcMotion())
+            TransitionManager.beginDelayedTransition(binding.root, changeBounds)
+            if (isFlag) {
+                params.gravity =Gravity.TOP or Gravity.START
+            } else {
+                params.gravity = Gravity.BOTTOM or Gravity.END
 
-
-            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
-            if(isFlag){
-                params.height = LinearLayout.LayoutParams.MATCH_PARENT
-                (it as ImageView).scaleType = ImageView.ScaleType.CENTER_CROP
             }
-else{
-                params.height = LinearLayout.LayoutParams.WRAP_CONTENT
-                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-                it.layoutParams = params
+            binding.button.layoutParams = params
 
-}
-
-
+        }
     }
-}}
+}
---
+++ app/src/main/java/com/nasa_gallery/ui/view/animations/AnimationActivityZoom.kt
@@ -0,0 +1,59 @@
+package com.nasa_gallery.ui.view.animations
+
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Explode
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import com.nasa_gallery.R
+import com.nasa_gallery.databinding.ActivityAnimationSecondBinding
+import com.nasa_gallery.databinding.ActivityAnimationZoomBinding
+
+class AnimationActivityZoom : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAnimationZoomBinding
+    var isFlag = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAnimationZoomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.imageView.setOnClickListener {
+            isFlag = !isFlag
+            val params = it.layoutParams as LinearLayout.LayoutParams
+            val transitionSet = TransitionSet()
+val changeImageTransform = ChangeImageTransform()
+            val changeBounds = ChangeBounds()
+            changeBounds.duration =2000L
+            changeImageTransform.duration =2000L
+            transitionSet.addTransition(changeBounds) // важен порядок
+            transitionSet.addTransition(changeImageTransform)
+
+
+            TransitionManager.beginDelayedTransition(binding.root, transitionSet)
+            if(isFlag){
+                params.height = LinearLayout.LayoutParams.MATCH_PARENT
+                (it as ImageView).scaleType = ImageView.ScaleType.CENTER_CROP
+            }
+else{
+                params.height = LinearLayout.LayoutParams.WRAP_CONTENT
+                binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+                it.layoutParams = params
+
+}
+
+
+    }
+}}
