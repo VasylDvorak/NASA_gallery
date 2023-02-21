33bd87365441d158f2f7d7e198ac78e320116446
Vasyl <dvorak1981@yandex.ru>
2023-02-21 22:35:47 +0300

    Object Animation

--- app/src/main/java/com/nasa_gallery/ui/view/animations/AnimationActivity.kt
+++ app/src/main/java/com/nasa_gallery/ui/view/animations/AnimationActivity.kt
@@ -1,49 +1,102 @@
 package com.nasa_gallery.ui.view.animations
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.os.Bundle
-
-import android.view.Gravity
-import android.widget.FrameLayout
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.transition.ArcMotion
-import androidx.transition.ChangeBounds
-import androidx.transition.TransitionManager
-import com.nasa_gallery.databinding.ActivityAnimationMixBinding
-import com.nasa_gallery.databinding.ActivityAnimationTraectoryBinding
+import com.nasa_gallery.databinding.ActivityAnimationComplicatedAnimationBinding
 
 class AnimationActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAnimationMixBinding
-    var isFlag = true
+    private lateinit var binding: ActivityAnimationComplicatedAnimationBinding
+    var isFlag = false
+    var duration = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationMixBinding.inflate(layoutInflater)
+        binding = ActivityAnimationComplicatedAnimationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-val titles:MutableList <String> = ArrayList()
-        for(i in 0..4){
+        val titles: MutableList<String> = ArrayList()
+        for (i in 0..4) {
             titles.add("Item $i")
         }
 
 
 
-        binding.button.setOnClickListener {
+        binding.fab.setOnClickListener {
             isFlag = !isFlag
-            TransitionManager.beginDelayedTransition(binding.root)
-            binding.transitionContainer.removeAllViews()
-titles.shuffle()
-titles.forEach{
-    binding.transitionContainer.addView(TextView(this).apply{
-        text = it
-        ViewCompat.setTransitionName(this, it) // задали псевданим
+            if (isFlag) {
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 0f, 675f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, -150f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, -270f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0.5f) .setDuration(duration).start()
+                binding.optionOneContainer.animate().alpha(1f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator, isReverse: Boolean) {
+                            binding.optionOneContainer.isClickable = true
+                            binding.optionOneContainer.setOnClickListener {
+                                Toast.makeText(applicationContext, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
 
-    })
+                    }
+                )
 
-}
+                binding.optionTwoContainer.animate().alpha(1f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator, isReverse: Boolean) {
+                            binding.optionTwoContainer.isClickable = true
+                            binding.optionTwoContainer.setOnClickListener {
+                                Toast.makeText(applicationContext, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+                    })
+
+            } else {
+                ObjectAnimator.ofFloat(binding.plusImageview, View.ROTATION, 675f, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionOneContainer, View.TRANSLATION_Y, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.optionTwoContainer, View.TRANSLATION_Y, 0f)
+                    .setDuration(duration).start()
+                ObjectAnimator.ofFloat(binding.transparentBackground, View.ALPHA, 0f) .setDuration(duration).start()
+                binding.optionOneContainer.animate().alpha(0f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator, isReverse: Boolean) {
+                            binding.optionOneContainer.isClickable = false
+                            binding.optionOneContainer.setOnClickListener {
+                                Toast.makeText(applicationContext, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+                    })
+
+                binding.optionTwoContainer.animate().alpha(0f).setDuration(duration).setListener(
+                    object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator, isReverse: Boolean) {
+                            binding.optionTwoContainer.isClickable = false
+                            binding.optionTwoContainer.setOnClickListener {
+                                Toast.makeText(applicationContext, "Option 1", Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+
+                    })
+
+
+            }
         }
     }
 }
