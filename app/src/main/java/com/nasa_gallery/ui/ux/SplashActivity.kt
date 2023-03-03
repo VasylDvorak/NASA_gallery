c4e625d6bea8e5ec8e2ac346c73dce46d94fa557
Vasyl <dvorak1981@yandex.ru>
2023-03-03 19:53:25 +0300

    решение для визуализхации загрузки/ обработки данных на старте

--- app/src/main/java/com/nasa_gallery/ui/ux/SplashActivity.kt
+++ app/src/main/java/com/nasa_gallery/ui/ux/SplashActivity.kt
@@ -1,9 +1,15 @@
 package com.nasa_gallery.ui.ux
 
 
+import android.animation.ObjectAnimator
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 
 import com.nasa_gallery.R
 
@@ -12,6 +18,16 @@ class SplashActivity : AppCompatActivity() { //TODO HW single-activity пыта�
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.IndigoTheme)
+        setContentView(R.layout.activity_splash)
+        findViewById<AppCompatImageView>(R.id.icon).animate().rotation(720f).setDuration(2000L).start()
+
+     //   ObjectAnimator.ofFloat(findViewById<AppCompatImageView>(R.id.icon), View.ROTATION, 720f).setDuration(2000L).start()
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+    startActivity(Intent(this, UXActivity::class.java))
+    finish()
+},2000L)
 
     }
 }
